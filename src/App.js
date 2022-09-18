9b9df840739db52d35280bc7362f0e01ebfe27cf
q <commitment1511@outlook.com>
2022-09-18 23:22:18 +0700

    gglogin

--- src/App.js
+++ src/App.js
@@ -31,3 +31,84 @@ function App() {
 }
 
 export default App
+
+// import React, { useState, useEffect } from "react"
+// import { GoogleLogin, GoogleLogout } from "react-google-login"
+// import { gapi } from "gapi-script"
+
+// function App() {
+//     const [profile, setProfile] = useState([])
+//     const clientId =
+//         "434535066606-gib6cr8c72avd4jt6s3h04ai21ra0leh.apps.googleusercontent.com"
+//     useEffect(() => {
+//         const initClient = () => {
+//             gapi.client.init({
+//                 clientId: clientId,
+//                 scope: ""
+//             })
+//         }
+//         gapi.load("client:auth2", initClient)
+//     })
+
+//     const onSuccess = (res) => {
+//         console.log("sucess:", res)
+//         setProfile(res.profileObj)
+//         // fetch("http://localhost:8081/gglogin", {
+//         //     method: "POST", // or 'PUT'
+//         //     headers: {
+//         //         "Content-Type": "application/json"
+//         //     },
+//         //     body: JSON.stringify({
+//         //         token: res.tokenId
+//         //     })
+//         // })
+//         //     .then((response) => response.json())
+//         //     .then((data) => {
+//         //         console.log("Success:", data)
+//         //     })
+//         //     .catch((error) => {
+//         //         console.error("Error:", error)
+//         //     })
+//     }
+
+//     const onFailure = (err) => {
+//         console.log("failed", err)
+//     }
+
+//     const logOut = () => {
+//         setProfile(null)
+//     }
+
+//     return (
+//         <div>
+//             <h2>React Google Login</h2>
+//             <br />
+//             <br />
+//             {profile ? (
+//                 <div>
+//                     <img src={profile.imageUrl} alt="user image" />
+//                     <h3>User Logged in</h3>
+//                     <p>Name: {profile.name}</p>
+//                     <p>Email Address: {profile.email}</p>
+//                     <br />
+//                     <br />
+//                     <GoogleLogout
+//                         clientId={clientId}
+//                         buttonText="Log out"
+//                         onLogoutSuccess={logOut}
+//                     />
+//                 </div>
+//             ) : (
+//                 <GoogleLogin
+//                     clientId={clientId}
+//                     buttonText="Sign in with Google"
+//                     onSuccess={onSuccess}
+//                     onFailure={onFailure}
+//                     cookiePolicy={"single_host_origin"}
+//                     isSignedIn={true}
+//                 />
+//             )}
+//         </div>
+//     )
+// }
+// export default App
